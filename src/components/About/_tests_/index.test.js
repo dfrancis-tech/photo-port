f94d77717f68cfb90b32ac5df174726a074bce0e
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-20 23:20:24 -0400

    execute passing test on About component

---
+++ src/components/About/_tests_/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// retrieve functions from React Testing Library
+import { render, cleanup } from '@testing-library/react';
+// import extend-expect library from jest-dom package
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+// call cleanup utility function to keep things clean
+afterEach(cleanup);
+
+// declare the component we are testing
+describe('About component', () => {
+  // First Test
+  it('renders', () => {
+    render(<About />);
+  });
+
+  // Second Test
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
