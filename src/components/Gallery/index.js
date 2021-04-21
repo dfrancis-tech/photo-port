ba53d516cdf463650a49704302ddcd9857e2e09c
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-21 16:49:26 -0400

    conditionally render gallery pages

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -11,7 +11,7 @@ function Gallery(props) {
   return (
     <section>
       <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.name}</p>
+      <p>{currentCategory.description}</p>
       <div className="flex-row">
           <img
             src={photo}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,34 +1,47 @@
-import React from 'react';
+//import React, { useState } from "react";
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-    const categories = [
-        {
-          name: "commercial",
-          description: "Photos of grocery stores, food trucks, and other commercial projects"
-        },
-        { 
-            name: "portraits", 
-            description: "Portraits of people in my life" 
-        },
-        { 
-            name: "food", 
-            description: "Delicious delicacies" 
-        },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature"
-        }
-    ];
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
 
-    function categorySelected(name) {
-        console.log(`${name} clicked`);
-    };
+    // to update the tab on the browser to reflect the user's category selection
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+      }, [currentCategory]);
+    //const [currentCategory, setCurrentCategory] = useState(categories[0]);
+    // const [categories] = useState([
+    //     {
+    //       name: "commercial",
+    //       description: "Photos of grocery stores, food trucks, and other commercial projects"
+    //     },
+    //     { 
+    //         name: "portraits", 
+    //         description: "Portraits of people in my life" 
+    //     },
+    //     { 
+    //         name: "food", 
+    //         description: "Delicious delicacies" 
+    //     },
+    //     {
+    //       name: "landscape",
+    //       description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+    //     }
+    // ]);
+
+    // function categorySelected(name) {
+    //     console.log(`${name} clicked`);
+    // };
 
     return (
         <header className="flex-row px-1">
             <h2>
                 <a data-testid="link" href="/">
-                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                <span role="img" aria-label="camera">📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -38,13 +51,15 @@ function Nav() {
                             About me
                         </a>
                     </li>
-                    <li>
+                    <li className="mx-2">
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li className="mx-1" key={category.name}>
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                        //<li className="mx-1" key={category.name}>
+                        // <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                            <span onClick={() => {setCurrentCategory(category)}}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
